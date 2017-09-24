05d47c3bfb5e30942703caf67a85d55efca49a50
Kashif Minhaj <dailypctips@gmail.com>
2017-09-24 15:30:41 +0530

    scripts: add typings for mocha and pretests

---
+++ test/index.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import { hello } from '../src/index';
+
+// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
+import 'mocha';
+
+describe('Hello function', () => {
+  it('should return hello world', () => {
+    const result = hello();
+    expect(result).to.equal('Hello World!');
+  });
+});
