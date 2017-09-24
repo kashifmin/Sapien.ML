79dc44864d7dc93b3f73b752d5eb415bc6039c89
Daksh <hello@dak.sh>
2017-09-24 14:40:14 +0530

    Delete index.ts.js

--- test/index.ts.js
+++
@@ -1,8 +0,0 @@
-var build = require('../src/index.ts')
-var expect = require('chai').expect;
-
-describe('hello', () => {
-    it('should export a function', () => {
-        expect(build).to.be.a('function')
-    })
-})
