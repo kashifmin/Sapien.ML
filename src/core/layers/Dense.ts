bda132c9b807f5f3de5bd6eb739d9f7831c5d555
Daksh <hello@dak.sh>
2017-10-17 10:36:20 +0530

    Cleaned for Better Code Hub

--- src/core/layers/Dense.ts
+++ src/core/layers/Dense.ts
@@ -1,8 +1,8 @@
 import { Layer } from 'core/layers'
 import { Tensor } from 'core/types'
 
-export class Dense extends Layer{
-    feedForward():Tensor{
+export class Dense extends Layer {
+    feedForward() : Tensor {
 
         //Todo:implement dense layer
         return new Tensor()
