dba28e46238c5454329d5d61b50312e8d1439d6a
junaid <junaid1460@gmail.com>
2017-10-17 19:22:27 +0530

    Changed signature and removed comment for bettercode

--- src/core/layers/Dense.ts
+++ src/core/layers/Dense.ts
@@ -2,9 +2,8 @@ import { Layer } from 'core/layers'
 import { Tensor } from 'core/types'
 
 export class Dense extends Layer{
-    feedForward():Tensor{
-
-        //Todo:implement dense layer
-        return new Tensor()
+    feedForward(input:Tensor):Tensor{
+        
+        return new Tensor();
     }
 }
